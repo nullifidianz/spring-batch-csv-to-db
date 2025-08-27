3522df14d8a7a3d599426e991b08cad67a82a9b0
João Paulo Paggi <nullifidianz@gmail.com>
2025-08-27 10:55:42 -0300

    initial commit

---
+++ src/main/java/com/nullifidianz/spring_batch_products/SpringBatchProductsApplication.java
@@ -0,0 +1,13 @@
+package com.nullifidianz.spring_batch_products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchProductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nullifidianz/spring_batch_products/SpringBatchProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nullifidianz.spring_batch_products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
